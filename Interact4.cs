b777bf1b37bb3cbd8467b171031ed58e300367b2
Aryaman-Dobhal <50620014+Aryaman-Dobhal@users.noreply.github.com>
2019-05-14 16:26:50 +0530

    Add files via upload
    Scripts for plane movement

---
+++ Interact4.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interact4 : MonoBehaviour {
+
+	public float gTime=2f;
+	private bool gaze=false;
+	private float timer;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (gaze) {
+			timer = timer + Time.deltaTime;
+		}
+		if(!gaze){
+			timer = 0f;
+		}
+		if (timer > gTime) {
+			SceneManager.LoadScene ("t4");
+		}
+	}
+
+	public void PointerEnter(){
+		Debug.Log ("enter");
+		gaze = true;
+		timer = 0f;
+
+	}
+
+	public void PointerExit(){
+		Debug.Log ("exit");
+		gaze = false;
+	}
+
+}
+
---
+++ Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Camera camera;
+    public float speed;
+    // Use this for initialization
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + camera.transform.forward * speed * Time.deltaTime;
+        speed = speed + Time.deltaTime;
+    }
+}
